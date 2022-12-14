cc6e3e82804b07803e79a217f115f120f4026bf0
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2022-12-13 18:29:59 -0800

    Added Absolute X and Y

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -31,25 +31,71 @@ namespace CursorLocation
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.xValue = new System.Windows.Forms.Label();
-            this.yValue = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.xLabel = new System.Windows.Forms.Label();
+            this.xValue = new System.Windows.Forms.Label();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PrimaryScreenXValue = new System.Windows.Forms.Label();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
             this.yLabel = new System.Windows.Forms.Label();
+            this.yValue = new System.Windows.Forms.Label();
+            this.flowLayoutPanel5 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PrimaryScreenYValue = new System.Windows.Forms.Label();
             this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
+            this.flowLayoutPanel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Controls.Add(this.xLabel);
-            this.flowLayoutPanel1.Controls.Add(this.xValue);
-            this.flowLayoutPanel1.Controls.Add(this.yLabel);
-            this.flowLayoutPanel1.Controls.Add(this.yValue);
+            this.flowLayoutPanel1.Controls.Add(this.tableLayoutPanel1);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(186, 31);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(383, 115);
             this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel4, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel5, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(376, 109);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.xLabel);
+            this.flowLayoutPanel2.Controls.Add(this.xValue);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(182, 48);
+            this.flowLayoutPanel2.TabIndex = 7;
+            // 
+            // xLabel
+            // 
+            this.xLabel.AutoSize = true;
+            this.xLabel.Location = new System.Drawing.Point(3, 0);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(24, 20);
+            this.xLabel.TabIndex = 7;
+            this.xLabel.Text = "X:";
             // 
             // xValue
             // 
@@ -60,45 +106,108 @@ namespace CursorLocation
             this.xValue.TabIndex = 0;
             this.xValue.Text = "label1";
             // 
-            // yValue
+            // flowLayoutPanel3
             // 
-            this.yValue.AutoSize = true;
-            this.yValue.Location = new System.Drawing.Point(120, 0);
-            this.yValue.Name = "yValue";
-            this.yValue.Size = new System.Drawing.Size(51, 20);
-            this.yValue.TabIndex = 1;
-            this.yValue.Text = "label1";
+            this.flowLayoutPanel3.Controls.Add(this.label1);
+            this.flowLayoutPanel3.Controls.Add(this.PrimaryScreenXValue);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(3, 57);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(182, 49);
+            this.flowLayoutPanel3.TabIndex = 8;
             // 
-            // xLabel
+            // label1
             // 
-            this.xLabel.AutoSize = true;
-            this.xLabel.Location = new System.Drawing.Point(3, 0);
-            this.xLabel.Name = "xLabel";
-            this.xLabel.Size = new System.Drawing.Size(24, 20);
-            this.xLabel.TabIndex = 2;
-            this.xLabel.Text = "X:";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "AbsoluteX:";
+            // 
+            // PrimaryScreenXValue
+            // 
+            this.PrimaryScreenXValue.AutoSize = true;
+            this.PrimaryScreenXValue.Location = new System.Drawing.Point(96, 0);
+            this.PrimaryScreenXValue.Name = "PrimaryScreenXValue";
+            this.PrimaryScreenXValue.Size = new System.Drawing.Size(51, 20);
+            this.PrimaryScreenXValue.TabIndex = 4;
+            this.PrimaryScreenXValue.Text = "label1";
+            this.PrimaryScreenXValue.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.Controls.Add(this.yLabel);
+            this.flowLayoutPanel4.Controls.Add(this.yValue);
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(191, 3);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(182, 48);
+            this.flowLayoutPanel4.TabIndex = 9;
             // 
             // yLabel
             // 
             this.yLabel.AutoSize = true;
-            this.yLabel.Location = new System.Drawing.Point(90, 0);
+            this.yLabel.Location = new System.Drawing.Point(3, 0);
             this.yLabel.Name = "yLabel";
             this.yLabel.Size = new System.Drawing.Size(24, 20);
             this.yLabel.TabIndex = 3;
             this.yLabel.Text = "Y:";
             // 
+            // yValue
+            // 
+            this.yValue.AutoSize = true;
+            this.yValue.Location = new System.Drawing.Point(33, 0);
+            this.yValue.Name = "yValue";
+            this.yValue.Size = new System.Drawing.Size(51, 20);
+            this.yValue.TabIndex = 1;
+            this.yValue.Text = "label1";
+            // 
+            // flowLayoutPanel5
+            // 
+            this.flowLayoutPanel5.Controls.Add(this.label2);
+            this.flowLayoutPanel5.Controls.Add(this.PrimaryScreenYValue);
+            this.flowLayoutPanel5.Location = new System.Drawing.Point(191, 57);
+            this.flowLayoutPanel5.Name = "flowLayoutPanel5";
+            this.flowLayoutPanel5.Size = new System.Drawing.Size(182, 49);
+            this.flowLayoutPanel5.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "AbsoluteY:";
+            // 
+            // PrimaryScreenYValue
+            // 
+            this.PrimaryScreenYValue.AutoSize = true;
+            this.PrimaryScreenYValue.Location = new System.Drawing.Point(96, 0);
+            this.PrimaryScreenYValue.Name = "PrimaryScreenYValue";
+            this.PrimaryScreenYValue.Size = new System.Drawing.Size(51, 20);
+            this.PrimaryScreenYValue.TabIndex = 5;
+            this.PrimaryScreenYValue.Text = "label1";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(186, 31);
+            this.ClientSize = new System.Drawing.Size(383, 115);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.TopMost = true;
             this.flowLayoutPanel1.ResumeLayout(false);
-            this.flowLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.flowLayoutPanel4.PerformLayout();
+            this.flowLayoutPanel5.ResumeLayout(false);
+            this.flowLayoutPanel5.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -108,8 +217,17 @@ namespace CursorLocation
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Label xValue;
         private System.Windows.Forms.Label yValue;
-        private System.Windows.Forms.Label xLabel;
         private System.Windows.Forms.Label yLabel;
+        private System.Windows.Forms.Label PrimaryScreenXValue;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label PrimaryScreenYValue;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using WindowsInput;
 
 namespace CursorLocation
 {
@@ -27,10 +29,68 @@ namespace CursorLocation
         {
             yValue.Text = Cursor.Position.Y.ToString();
             xValue.Text = Cursor.Position.X.ToString();
+            PrimaryScreenXValue.Text = XCoordinatesToRatio(Int32.Parse(Cursor.Position.X.ToString())).ToString();
+            PrimaryScreenYValue.Text = YCoordinatesToRatio(Int32.Parse(Cursor.Position.Y.ToString())).ToString();
         }
         private void CoordTimer_Tick(object sender, EventArgs e)
         {
             xValue.Invoke(new labelChanger(UpdateCoords));
+        }
+        public static void moveMouseToCoords(int inputX, int inputY, InputSimulator input)
+        {
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+            ushort newX = 0;
+            ushort newY = 0;
+            if (inputX > screenWidth || inputY > screenWidth || inputX < 0 || inputY < 0)
+            {
+                throw new Exception("Invalid input, method only works on the primary display");
+            }
+            newX = (ushort)(((double)inputX) * (((double)ushort.MaxValue) / ((double)(screenWidth - 1))));
+            newY = (ushort)(((double)inputY) * (((double)ushort.MaxValue) / ((double)(screenHeight - 1))));
+
+            input.Mouse.MoveMouseTo(newX, newY);
+
+        }
+        public static ushort XCoordinatesToRatio(int inputX ) {
+            int minX= SystemInformation.VirtualScreen.X; //the leftmost coord possible for x
+
+            int inputXModified = inputX;// + minX;
+                inputXModified += -1*minX;
+
+            int screenWidth = SystemInformation.VirtualScreen.Width;
+            /*
+            int screenWidth2 = Screen.PrimaryScreen.Bounds.Width;
+            int test = SystemInformation.VirtualScreen.X;
+            int test2 = SystemInformation.VirtualScreen.Y;
+            */
+            ushort newX = 0;
+            if (inputX > minX+screenWidth ||  inputX < minX )
+            {
+                return 0;
+                throw new Exception("Invalid input, method only works on the primary display");
+            }
+            newX = (ushort)(((double)inputXModified) * (((double)ushort.MaxValue) / ((double)(screenWidth-1 ))));
+            return newX;
+        }
+        public static ushort YCoordinatesToRatio(int inputY ) { 
+            int minY= SystemInformation.VirtualScreen.Y;
+            int inputYModified = inputY;
+                inputYModified += -1*minY;
+            int screenHeight = SystemInformation.VirtualScreen.Height;
+            ushort newY = 0;
+            if ( inputY > minY+screenHeight||  inputY < minY)
+            {
+                return 0;
+                throw new Exception("Invalid input, method only works on the primary display");
+            }
+            newY = (ushort)(((double)inputYModified) * (((double)ushort.MaxValue) / ((double)(screenHeight - 1))));
+            return newY;
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
